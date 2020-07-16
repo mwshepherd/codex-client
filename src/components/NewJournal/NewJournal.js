6c4288e998c1732c554816653e08e3e08f012775
Michael Shepherd <mwshepherd11@gmail.com>
2020-07-16 21:34:14 +1000

    added draft JS, set up initial editor page

--- src/components/NewJournal/NewJournal.js
+++ src/components/NewJournal/NewJournal.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
 class NewJournal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { editorState: EditorState.createEmpty() };
+    this.onChange = (editorState) => this.setState({ editorState });
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
+  _onItalicClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+  }
+
   render() {
-    return <div>New Journal Page</div>;
+    return (
+      <div>
+        <h1>New Journal Page</h1>
+        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+        <button onClick={this._onItalicClick.bind(this)}>Italic</button>
+        <Editor editorState={this.state.editorState} handleKeyCommand={this.handleKeyCommand} onChange={this.onChange} />
+      </div>
+    );
   }
 }
 
