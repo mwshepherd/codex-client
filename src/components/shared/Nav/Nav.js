04164e1041a2ea713211da1e1296f2b4653c2a3e
Michael Shepherd <mwshepherd11@gmail.com>
2020-07-17 14:43:41 +1000

    nav bar responsive

--- src/components/shared/Nav/Nav.js
+++ src/components/shared/Nav/Nav.js
@@ -7,7 +7,7 @@ class Nav extends Component {
     return (
       <div className={`left-panel ${this.props.navState}`}>
         <nav className="nav-main">
-          <ul>
+          <ul className="quick-links">
             <li onClick={() => this.props.setCurrentPage('new-journal')}>
               <div className="link-wrapper">
                 <div className="icon">
@@ -30,6 +30,57 @@ class Nav extends Component {
                   <i className="far fa-calendar-check"></i>
                 </div>
                 <div className={navExpanded ? `nav-link show` : `nav-link`}>New Goal</div>
+              </div>
+            </li>
+          </ul>
+
+          <ul className="regular-links">
+            <li onClick={() => this.props.setCurrentPage('home')}>
+              <div className="link-wrapper">
+                <div className="icon">
+                  <i className="fas fa-home"></i>
+                </div>
+                <div className={navExpanded ? `nav-link show` : `nav-link`}>Home</div>
+              </div>
+            </li>
+            <li onClick={() => this.props.setCurrentPage('journals')}>
+              <div className="link-wrapper">
+                <div className="icon">
+                  <i className="fas fa-book"></i>
+                </div>
+                <div className={navExpanded ? `nav-link show` : `nav-link`}>Journals</div>
+              </div>
+            </li>
+            <li onClick={() => this.props.setCurrentPage('bookmarks')}>
+              <div className="link-wrapper">
+                <div className="icon">
+                  <i className="fas fa-link"></i>
+                </div>
+                <div className={navExpanded ? `nav-link show` : `nav-link`}>Bookmarks</div>
+              </div>
+            </li>
+            <li onClick={() => this.props.setCurrentPage('goals')}>
+              <div className="link-wrapper">
+                <div className="icon">
+                  <i className="fas fa-clipboard-list"></i>
+                </div>
+                <div className={navExpanded ? `nav-link show` : `nav-link`}>Goals</div>
+              </div>
+            </li>
+            <li onClick={() => this.props.setCurrentPage('timer')}>
+              <div className="link-wrapper">
+                <div className="icon">
+                  <i className="far fa-clock"></i>
+                </div>
+                <div className={navExpanded ? `nav-link show` : `nav-link`}>Timer</div>
+              </div>
+            </li>
+            <li onClick={() => this.props.setCurrentPage('analytics')}>
+              <div className="link-wrapper">
+                <div className="icon">
+                  <i className="far fa-chart-bar"></i>
+                </div>
+                <div className={navExpanded ? `nav-link show` : `nav-link`}>Analytics</div>
               </div>
             </li>
           </ul>
