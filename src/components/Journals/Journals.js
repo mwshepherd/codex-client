6a6cf702107eba208d468dbf8fd33942a22c04bf
Michael Shepherd <mwshepherd11@gmail.com>
2020-07-17 16:40:18 +1000

    get currentUser journal data, also fixed responsive nav

--- src/components/Journals/Journals.js
+++ src/components/Journals/Journals.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
+import { backendServer } from '../shared/constants';
 
 class Journals extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      journals: {},
+    };
+
+    this.getUsersJournals = this.getUsersJournals.bind(this);
+    this.renderJournalEntries = this.renderJournalEntries.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.getUsersJournals();
+  }
+
+  async getUsersJournals() {
+    const response = await fetch(`${backendServer}/journals`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    });
+    const data = await response.json();
+    this.setState({ journals: data.journals });
+  }
+
+  renderJournalEntries(journal) {
+    return (
+      <div className="journal-entry">
+        <div className="journal-entry__title">{journal.title}</div>
+      </div>
+    );
+  }
+
   render() {
-    return <div>Journals</div>;
+    console.log(this.state);
+    return (
+      <>
+        <div>Journals</div>
+      </>
+    );
   }
 }
 
