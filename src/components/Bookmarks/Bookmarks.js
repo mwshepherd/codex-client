92a6981e4863c3c5bd4f54c685d488453db51388
Emma Bindi <emma.bindi@outlook.com>
2020-07-21 10:06:23 +1000

    Bookmark view all index setup - now need to add sorting and pagination)

--- src/components/Bookmarks/Bookmarks.js
+++ src/components/Bookmarks/Bookmarks.js
@@ -1,8 +1,76 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import './Bookmarks.scss';
+import { backendServer } from '../shared/constants';
+
 
 class Bookmarks extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    bookmarks: []
+  };
+
+  this.getUsersBookmarks = this.getUsersBookmarks.bind(this);
+  this.renderBookmarks = this.renderBookmarks.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.getUsersBookmarks();
+  }
+
+  async getUsersBookmarks() {
+    const response = await fetch(`${backendServer}/bookmarks`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    });
+    const data = await response.json();
+    this.setState({ bookmarks: data.bookmarks })
+  }
+
+  renderBookmarks(bookmark) {
+    console.log(bookmark);
+    // const date = moment(bookmark.created_at).format('dddd, MMMM Do YYYY, h:mm:ss a');
+    const categories = 
+    bookmark.categories && 
+    bookmark.categories.map((category) => {
+      return (
+        <span key={category.id} className="category">#{category.name}</span>
+      );
+    });
+
+    return (
+      <div key={bookmark.id} className="bookmark">
+        <div className="bookmark-title" onClick={() => {
+          // need to insert re the current page bit
+        }}>
+        {bookmark.title}
+        </div>
+        <div className="bookmark-url"><a href={bookmark.url}>{bookmark.url}</a></div>
+        <div className="bookmark-categories">{categories}</div>
+        {/* <div>{date}</div> */}
+      </div>
+    )
+  }
+
   render() {
-    return <div>Bookmarks page</div>;
+    console.log(this.state);
+    const { bookmarks } = this.state;
+    console.log(bookmarks);
+    return (
+      <>
+        <h1>Bookmarks</h1>
+        <div className="bookmarks-table">
+          <div className="bookmark">
+            <div className="bookmark-title">Title</div>
+            <div className="bookmark-url-title">URL</div>
+            <div className="bookmark-categories">Category</div>
+          </div>
+        {bookmarks.length && bookmarks.map((bookmark) => this.renderBookmarks(bookmark))}
+        </div>
+      </>
+    );
   }
 }
 
