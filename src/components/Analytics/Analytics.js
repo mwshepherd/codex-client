cf66817e659d0a53d22ec321d3d499424c42d765
Michael Shepherd <mwshepherd11@gmail.com>
2020-07-24 11:21:14 +1000

    setup basic charts in analytics page

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { LineChart, PieChart, ColumnChart } from 'react-chartkick';
+import 'chart.js';
+import './Analytics.scss';
+
+const data = {
+  Journal: 2,
+  Bookmarks: 15,
+  Goals: 10,
+};
 
 export default class Analytics extends Component {
   render() {
     return (
       <div>
         <h1>Analytics</h1>
+        <div className="demo-chart">
+          <LineChart data={data} width="100%" />
+          <ColumnChart data={data} width="100%" />
+          <PieChart data={data} width="100%" />
+        </div>
+
+        {/* <PieChart data={data} width="100%" /> */}
       </div>
     );
   }
