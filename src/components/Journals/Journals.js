140ccb0d9c01c4cafb2b133a495f4e5a31ba6a03
Michael Shepherd <mwshepherd11@gmail.com>
2020-07-25 01:07:01 +1000

    journals page styling, fixing styling to pagination btns

--- src/components/Journals/Journals.js
+++ src/components/Journals/Journals.js
@@ -23,18 +23,17 @@ class Journals extends Component {
     const date = moment(journal.created_at).format('D/MM/YYYY');
 
     return (
-      <div key={journal.id} className="journal__entry">
-        <Link
-          to={{
-            pathname: `/dashboard/journals/${journal.id}`,
-          }}
-          className="journal__entry-title"
-        >
-          {journal.title}
-        </Link>
+      <Link
+        to={{
+          pathname: `/dashboard/journals/${journal.id}`,
+        }}
+        key={journal.id}
+        className="journal__entry"
+      >
+        <div className="journal__entry-title">{journal.title}</div>
         <div className="journal__entry-categories">{journal.category.name}</div>
         <div className="journal__entry-date">{date}</div>
-      </div>
+      </Link>
     );
   }
 
@@ -49,28 +48,28 @@ class Journals extends Component {
             <h1>Journals</h1>
             <div className="add-journal">Add</div>
           </div>
+          <div className="pagination-btns">
+            <button onClick={() => prevPage('journals')}>Prev</button>
+            <div className="total-pages">
+              {currPage} / {totalPages}
+            </div>
+            <button onClick={() => nextPage('journals')}>Next</button>
+          </div>
           <div className="journal__columns">
             <div className="journal__title" onClick={() => this.props.sortByType('title', page)}>
               <span>Title</span>
-              <i class="fas fa-sort"></i>
+              <i className="fas fa-sort"></i>
             </div>
             <div className="journal__category" onClick={() => this.props.sortByType('category', page)}>
               <span>Category</span>
-              <i class="fas fa-sort"></i>
+              <i className="fas fa-sort"></i>
             </div>
             <div className="journal-date" onClick={() => this.props.sortByType('created_at', page)}>
               <span>Date</span>
-              <i class="fas fa-sort"></i>
+              <i className="fas fa-sort"></i>
             </div>
           </div>
           {journals && journals.map((journal) => this.renderJournalEntries(journal))}
-        </div>
-        <div className="pagination-btns">
-          <button onClick={() => prevPage('journals')}>Prev</button>
-          <div className="total-pages">
-            {currPage} / {totalPages}
-          </div>
-          <button onClick={() => nextPage('journals')}>Next</button>
         </div>
       </>
     );
