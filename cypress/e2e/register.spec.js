128b64bd0e30f7de681ec01ecbaba41964d2960a
Emma Bindi <emma.bindi@outlook.com>
2020-07-27 08:36:56 +1000

    Adding faker generation for username

---
+++ cypress/e2e/register.spec.js
@@ -0,0 +1,13 @@
+import { userBuilder } from "../support/generate";
+
+// describe("signing up as user", () => {
+//   it("should register a new user", () => {
+//     const user = userBuilder();
+//     cy.visit("/");
+//     cy.findByTestId("sign-up").click();
+//     cy.typeInLoginCredentials(user.email, user.password)
+//     cy.get('form').submit()
+//     cy.url().should("eq", "http://localhost:8080/bookmarks");
+//     cy.findByTestId("no-bookmarks", {timeout: 500}).should("exist")
+//   });
+// });
